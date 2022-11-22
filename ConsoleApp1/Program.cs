9d2c342cf44e1984aaf5308e8938d74b78d6775a
TaqiAhmed <taqiahmed339@gmail.com>
2022-11-22 14:38:48 +0500

    Printing table of number of user inputted!

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,3 +2,7 @@
 Console.WriteLine("Hello, World!");
 int num1=Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Num1: "+num1);
+for(int i = 1; i <= 10; i++)
+{
+    Console.WriteLine(num1 + " x " + i + " = " + num1 * i);
+}
