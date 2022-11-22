420328d2255c9f99b150012e8a96b55bfacc382c
TaqiAhmed <taqiahmed339@gmail.com>
2022-11-22 14:55:14 +0500

    printing five times of a user input number

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,11 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-int num1=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Num1: "+num1);
-for(int i = 1; i <= 10; i++)
+﻿public class Program
 {
-    Console.WriteLine(num1 + " x " + i + " = " + num1 * i);
+    public static int get5Times(int num)
+    {
+        return num * 5;
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Num1: " + num1);
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(num1 + " x " + i + " = " + num1 * i);
+        }
+
+
+        Console.WriteLine("Hope you enjoyed the lesson!");
+
+        int fiveTimesNo = get5Times(num1);
+        Console.WriteLine(fiveTimesNo);
+
+        
+    }
+    
 }
-
-
-Console.WriteLine("Hope you enjoyed the lesson!");
